6bdffd1ca661e15e29175724d6259fb96973ccf7
Joyce <zhao.xue1@husky.neu.edu>
2019-12-06 23:38:26 -0500

    error when importing canvasjs

--- assets/js/components/ContentArea.js
+++ assets/js/components/ContentArea.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import CanvasJS from 'canvasjs';
+var CanvasJSChart = CanvasJS.Chart;
 
 class ContentArea extends Component {
   constructor(props) {
@@ -6,8 +8,31 @@ class ContentArea extends Component {
   }
 
   render() {
-    return <div>ContentArea</div>;
+    const options = {
+      title: {
+        text: "Basic Column Chart in React"
+      },
+      data: [{				
+                type: "column",
+                dataPoints: [
+                    { label: "Apple",  y: 10  },
+                    { label: "Orange", y: 15  },
+                    { label: "Banana", y: 25  },
+                    { label: "Mango",  y: 30  },
+                    { label: "Grape",  y: 28  }
+                ]
+       }]
+   }
+		
+   return (
+      <div>
+        <CanvasJSChart options = {options}
+            /* onRef = {ref => this.chart = ref} */
+        />
+      </div>
+    );
   }
+  
 }
 
 export default ContentArea;
