f152e3074ae2acb53f4f4614e8f91f113ecb259b
Joyce <zhao.xue1@husky.neu.edu>
2019-12-08 18:10:12 -0500

    chart new styles

--- assets/js/components/Chart.js
+++ assets/js/components/Chart.js
@@ -4,19 +4,21 @@ import CanvasJSReact from '../lib/canvasjs-2.3.2/canvasjs.react';
 const Chart = ({weather, trip, enabledFactors}) => {
   const CanvasJSChart = CanvasJSReact.CanvasJSChart;
   const corssFactors = [
-    { label: 'Precipitation',    value: 'precipAccumulation',     color: '#C0504E', suffix: 'cm' },
-    { label: 'Temperature High', value: 'apparentTemperatureMax', color: '#4F81BC', suffix: '°F' },
-    { label: 'Temperature Low',  value: 'apparentTemperatureMin', color: '#44c320', suffix: '°F' },
-    { label: 'Humidity',         value: 'humidity',               color: '#36D7B7', suffix: ''   },
-    { label: 'Visibility',       value: 'visibility',             color: '#CDFFD8', suffix: ''   },
-    { label: 'Ozone',            value: 'ozone',                  color: '#FF5A52', suffix: ''   },
-    { label: 'UV Index',         value: 'uvIndex',                color: '#282829', suffix: ''   }
+    { label: 'Precipitation',    value: 'precipAccumulation',     color: '#f7acbc', suffix: 'cm' },
+    { label: 'Temperature High', value: 'apparentTemperatureMax', color: '#ffcc99', suffix: '°F' },
+    { label: 'Temperature Low',  value: 'apparentTemperatureMin', color: '#9b95c9', suffix: '°F' },
+    { label: 'Humidity',         value: 'humidity',               color: '#b2d235', suffix: ''   },
+    { label: 'Visibility',       value: 'visibility',             color: '#94d6da', suffix: ''   },
+    { label: 'Ozone',            value: 'ozone',                  color: '#999d9c', suffix: ''   },
+    { label: 'UV Index',         value: 'uvIndex',                color: '#c76968', suffix: ''   }
   ];
   // const colors = ['#C0504E', '#4F81BC', '#44c320', '#36D7B7', '#CDFFD8', '#FF5A52', '#282829', '#764BC4'];
+  // const tripColors = ['#5981b7', '#fcaf17', '#8552a1', '#cc333f', '#6666FF', '#FF5A52', '#282829', '#764BC4'];
   enabledFactors = corssFactors.filter(f => enabledFactors.includes(f.value));
   const dataForEnabledFactors = enabledFactors.map((f, i) => ({
     type: 'column',
     name: f.label,
+    color: f.color,
     axisYIndex: i,
     showInLegend: true,
     dataPoints: weather.map(w => ({
@@ -27,8 +29,9 @@ const Chart = ({weather, trip, enabledFactors}) => {
   const dataForTrips = {
     type: 'line',
     name: 'Rentals',
+    color: "#5981b7",
     axisYType: 'secondary',
-    // showInLegend: true,
+    showInLegend: true,
     yValueFormString: '#,##0.#',
     dataPoints: trip.map(t => ({
       label: t.date,
@@ -60,10 +63,12 @@ const Chart = ({weather, trip, enabledFactors}) => {
       text: "Click Legend to Hide or Unhide Data Series"
     }],
     axisX: {
-      title: "Dates"
+      title: "Dates",
+      titleFontSize: 13,
+      labelFontSize: 10,
     },
     axisY: enabledFactors.map(f => ({
-      title: f.label,
+      labelFontSize: 10,
       titleFontColor: f.color,
       lineColor: f.color,
       labelFontColor: f.color,
@@ -72,16 +77,19 @@ const Chart = ({weather, trip, enabledFactors}) => {
     })),
     axisY2: {
       title: "Rentals",
-      titleFontColor: "#764BC4",
-      lineColor: "#764BC4",
-      labelFontColor: "#764BC4",
-      tickColor: "#764BC4"
+      titleFontSize: 13,
+      labelFontSize: 10,
+      titleFontColor: "#5981b7",
+      lineColor: "#5981b7",
+      labelFontColor: "#5981b7",
+      tickColor: "#5981b7"
     },
     toolTip: {
       shared: true
     },
     legend: {
       cursor: "pointer",
+      reversed: true,
       itemclick: toggleDataSeries
     },
     data: [...dataForEnabledFactors, dataForTrips]
