07ec6a035086e0e9258cda3cc7c89d67bd32a898
Joyce <zhao.xue1@husky.neu.edu>
2019-12-08 22:10:57 -0500

    style and color

--- assets/js/components/Chart.js
+++ assets/js/components/Chart.js
@@ -4,19 +4,20 @@ import CanvasJSReact from '../lib/canvasjs-2.3.2/canvasjs.react';
 const Chart = ({weather, trip, enabledFactors, scatteredUserFilter, selectedRegions}) => {
   const CanvasJSChart = CanvasJSReact.CanvasJSChart;
   const corssFactors = [
-    { label: 'Precipitation',    value: 'precipAccumulation',     color: '#C0504E', suffix: 'cm' },
-    { label: 'Temperature High', value: 'apparentTemperatureMax', color: '#4F81BC', suffix: '°F' },
-    { label: 'Temperature Low',  value: 'apparentTemperatureMin', color: '#44c320', suffix: '°F' },
-    { label: 'Humidity',         value: 'humidity',               color: '#36D7B7', suffix: ''   },
-    { label: 'Visibility',       value: 'visibility',             color: '#CDFFD8', suffix: ''   },
-    { label: 'Ozone',            value: 'ozone',                  color: '#FF5A52', suffix: ''   },
-    { label: 'UV Index',         value: 'uvIndex',                color: '#282829', suffix: ''   }
+    { label: 'Precipitation',    value: 'precipAccumulation',     color: '#f7acbc', suffix: 'cm' },
+    { label: 'Temperature High', value: 'apparentTemperatureMax', color: '#ffcc99', suffix: '°F' },
+    { label: 'Temperature Low',  value: 'apparentTemperatureMin', color: '#9b95c9', suffix: '°F' },
+    { label: 'Humidity',         value: 'humidity',               color: '#b2d235', suffix: ''   },
+    { label: 'Visibility',       value: 'visibility',             color: '#94d6da', suffix: ''   },
+    { label: 'Ozone',            value: 'ozone',                  color: '#999d9c', suffix: ''   },
+    { label: 'UV Index',         value: 'uvIndex',                color: '#c76968', suffix: ''   }
   ];
-  // const colors = ['#C0504E', '#4F81BC', '#44c320', '#36D7B7', '#CDFFD8', '#FF5A52', '#282829', '#764BC4'];
+  const tripColors = ['#5981b7', '#fcaf17', '#8552a1', '#cc333f', '#6666FF'];
   enabledFactors = corssFactors.filter(f => enabledFactors.includes(f.value));
   const dataForEnabledFactors = enabledFactors.map((f, i) => ({
     type: 'column',
     name: f.label,
+    color: f.color,
     axisYIndex: i,
     showInLegend: true,
     dataPoints: weather.map(w => ({
@@ -28,21 +29,26 @@ const Chart = ({weather, trip, enabledFactors, scatteredUserFilter, selectedRegi
   var dataForTrips = [];
   if (scatteredUserFilter){
     const allFilters = {
-      Gender: [{label: 'Gender Unknown', value: '0'}, {label: 'Male', value: '1'}, {label: 'Female', value: '2'}],
+      Gender: [
+        {label: 'Gender Unknown', value: '0', color: '#5981b7'}, 
+        {label: 'Male', value: '1', color: '#fcaf17'}, 
+        {label: 'Female', value: '2', color: '#8552a1'}],
       Age: [
-        {label: '<= 20', value: '<= 20'},
-        {label: '20-30', value: '20-30'},
-        {label: '30-40', value: '30-40'},
-        {label: '40-50', value: '40-50'},
-        {label: '> 50',  value: '> 50' },
+        {label: '<= 20', value: '<= 20', color: '#5981b7'},
+        {label: '20-30', value: '20-30', color: '#fcaf17'},
+        {label: '30-40', value: '30-40', color: '#8552a1'},
+        {label: '40-50', value: '40-50', color: '#cc333f'},
+        {label: '> 50',  value: '> 50', color: '#6666FF'},
       ],
       Regions: selectedRegions
     };
-    // console.log('allfil', allFilters);
+    // console.log('selectedRegions', selectedRegions);
     dataForTrips = allFilters[scatteredUserFilter].map(
       p => ({
         type: 'line',
         name: p.label,
+        color: p.color,
+        markerType: "none",
         axisYType: 'secondary',
         showInLegend: true,
         yValueFormString: '#,##0.#',
@@ -54,8 +60,10 @@ const Chart = ({weather, trip, enabledFactors, scatteredUserFilter, selectedRegi
     dataForTrips = [{
       type: 'line',
       name: 'Rentals',
+      color: "#5981b7",
+      markerType: "none",
       axisYType: 'secondary',
-      // showInLegend: true,
+      showInLegend: true,
       yValueFormString: '#,##0.#',
       dataPoints: trip.map(t => ({
         label: t.date,
@@ -64,26 +72,6 @@ const Chart = ({weather, trip, enabledFactors, scatteredUserFilter, selectedRegi
     }];
   }
   console.log('dataForTrips', dataForTrips);
-  // const dataForTrips = {
-  //   type: 'line',
-  //   name: 'Rentals',
-  //   axisYType: 'secondary',
-  //   // showInLegend: true,
-  //   yValueFormString: '#,##0.#',
-  //   dataPoints: trip.map(t => ({
-  //     label: t.date,
-  //     y: parseInt(t.count)
-  //   }))
-  // };
-
-  // console.log('weather data points', enabledFactors.map(f => weather.map(w => ({
-  //   label: w.date,
-  //   y: w[f.value]
-  // }))));
-  // console.log('trip data points', trip.map(t => ({
-  //   label: t.date,
-  //   y: t.count
-  // })));
 
   const toggleDataSeries = (e) => {
     if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
@@ -94,16 +82,19 @@ const Chart = ({weather, trip, enabledFactors, scatteredUserFilter, selectedRegi
     e.chart.render();
   };
   const options = {
+    zoomEnabled: true,
     animationEnabled: true,
     title: {text: "Cross Factor Analysis Chart"},
     subtitles: [{
       text: "Click Legend to Hide or Unhide Data Series"
     }],
     axisX: {
-      title: "Dates"
+      title: "Dates",
+      titleFontSize: 13,
+      labelFontSize: 10,
     },
     axisY: enabledFactors.map(f => ({
-      title: f.label,
+      labelFontSize: 10,
       titleFontColor: f.color,
       lineColor: f.color,
       labelFontColor: f.color,
@@ -112,17 +103,20 @@ const Chart = ({weather, trip, enabledFactors, scatteredUserFilter, selectedRegi
     })),
     axisY2: {
       title: "Rentals",
-      titleFontColor: "#764BC4",
-      lineColor: "#764BC4",
-      labelFontColor: "#764BC4",
-      tickColor: "#764BC4"
+      titleFontSize: 13,
+      labelFontSize: 10,
+      titleFontColor: "#5981b7",
+      lineColor: "#5981b7",
+      labelFontColor: "#5981b7",
+      tickColor: "#5981b7"
     },
     toolTip: {
       shared: true
     },
     legend: {
       cursor: "pointer",
-      itemclick: toggleDataSeries
+      itemclick: toggleDataSeries,
+      reversed: true
     },
     data: [...dataForEnabledFactors, ...dataForTrips]
   };
